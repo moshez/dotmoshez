9a4c6c7821a86acb7a9b43a1431311cc1db4738b
Moshe Zadka <moshez@zadka.club>
2024-01-08 02:25:45 +0000

    checkpoint

--- src/dotmoshez/ssh.py
+++ src/dotmoshez/ssh.py
@@ -1,5 +1,4 @@
 import os
-import subprocess
 import pathlib
 
 from . import ENTRY_DATA
@@ -12,17 +11,26 @@ from gather.commands import add_argument
     add_argument("--host", required=True),
     add_argument("--user", required=True),
 )
-def sshirc(args): # pragma: no cover
+def sshirc(args):  # pragma: no cover
     execlp = getattr(args, "execlp", os.execlp)
     command = ["tmux", "at"]
-    dockerize = ["sudo", "docker", "exec", "-it", "-u", args.user, args.container] + command
+    dockerize = [
+        "sudo",
+        "docker",
+        "exec",
+        "-it",
+        "-u",
+        args.user,
+        args.container,
+    ] + command
     ssh_opts = []
     for key, value in dict(ServerAliveInterval=5, ServerAliveCountMax=2).items():
         ssh_opts.extend(["-o", f"{key}={value}"])
     sshize = ["ssh", "ssh", *ssh_opts, "-t", f"{args.user}@{args.host}"] + dockerize
     execlp(*sshize)
-    
-def parse_environment(env_path): # pragma: no cover
+
+
+def parse_environment(env_path):  # pragma: no cover
     try:
         content = env_path.read_text()
     except OSError:
@@ -38,7 +46,8 @@ def parse_environment(env_path): # pragma: no cover
             continue
         yield key, value
 
-def is_agent_up(env, safe_run): # pragma: no cover
+
+def is_agent_up(env, safe_run):  # pragma: no cover
     try:
         pid = env["SSH_AGENT_PID"]
     except KeyError:
@@ -54,21 +63,24 @@ def is_agent_up(env, safe_run): # pragma: no cover
             return True
     return False
 
-def bring_up_agent(env_path, run): # pragma: no cover
+
+def bring_up_agent(env_path, run):  # pragma: no cover
     results = run(["ssh-agent"], capture_output=True)
     agent_output = results.stdout
     agent_output = agent_output.replace("echo ", "# echo ")
     env_path.write_text(agent_output)
-        
+
+
 @ENTRY_DATA.register(
     add_argument("--no-dry-run", action="store_true", default=False),
-    add_argument("--env-path", default=pathlib.Path.home() / ".ssh" / "agent-environment"),
+    add_argument(
+        "--env-path", default=pathlib.Path.home() / ".ssh" / "agent-environment"
+    ),
     name="ssh-agent-env",
 )
-def ssh_agent_env(args): # pragma: no cover
+def ssh_agent_env(args):  # pragma: no cover
     args.env_path = pathlib.Path(args.env_path)
     env = dict(parse_environment(args.env_path))
     if not is_agent_up(env, args.safe_run):
         bring_up_agent(args.env_path, args.run)
     print(args.env_path.read_text())
-
