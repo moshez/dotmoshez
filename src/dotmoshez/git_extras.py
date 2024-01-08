bf9dc71a9566bfbfd48daf12aa4676e72b575b00
Moshe Zadka <moshez@zadka.club>
2024-01-08 17:31:10 +0000

    checkpoint

--- src/dotmoshez/git_extras.py
+++ src/dotmoshez/git_extras.py
@@ -1,3 +1,6 @@
+import pathlib
+import os
+
 from . import ENTRY_DATA
 from gather.commands import add_argument
 
@@ -18,3 +21,18 @@ def git_sync(args):  # pragma: no cover
     git("add", ".")
     git("commit", "-a", "-m", "checkpoint")
     git("push")
+
+
+@ENTRY_DATA.register(
+    add_argument("--no-dry-run", action="store_true", default=True),
+    add_argument("name"),
+    name="git-create",
+)
+def git_create(args):  # pragma: no cover
+    git = make_git(args)
+    target_dir = pathlib.Path(args.env["HOME"]) / "src" / args.name
+    target_dir.mkdir(parents=True)
+    args.env = dict(args.env, PWD=os.fspath(target_dir))
+    git("init", ".")
+    git("remote", "add", "origin", f"git@github.com:moshez/{args.name}")
+    git("push", "-u", "origin", "trunk")
