1016f8330bd55af07ccb9d5699f0ab5de518e7ef
Moshe Zadka <moshez@zadka.club>
2024-01-08 02:22:14 +0000

    checkpoint

--- src/dotmoshez/ssh.py
+++ src/dotmoshez/ssh.py
@@ -22,7 +22,7 @@ def sshirc(args): # pragma: no cover
     sshize = ["ssh", "ssh", *ssh_opts, "-t", f"{args.user}@{args.host}"] + dockerize
     execlp(*sshize)
     
-def parse_environment(env_path):
+def parse_environment(env_path): # pragma: no cover
     try:
         content = env_path.read_text()
     except OSError:
@@ -38,26 +38,34 @@ def parse_environment(env_path):
             continue
         yield key, value
 
-def is_agent_up(env, safe_run):
+def is_agent_up(env, safe_run): # pragma: no cover
     try:
         pid = env["SSH_AGENT_PID"]
     except KeyError:
         return False
-    results = safe_run(["ps", "-ef"])
+    results = safe_run(["ps", "-ef", "--width", "1000"])
     lines = iter(results.stdout.splitlines())
     fields = next(lines).split()
     for a_line in lines:
+        if "<defunct>" in a_line:
+            continue
         values = dict(zip(fields, a_line.split()))
         if values["PID"] == pid:
             return True
     return False
+
+def bring_up_agent(env_path, run): # pragma: no cover
+    results = run(["ssh-agent"], capture_output=True)
+    agent_output = results.stdout
+    agent_output = agent_output.replace("echo ", "# echo ")
+    env_path.write_text(agent_output)
         
 @ENTRY_DATA.register(
     add_argument("--no-dry-run", action="store_true", default=False),
     add_argument("--env-path", default=pathlib.Path.home() / ".ssh" / "agent-environment"),
     name="ssh-agent-env",
 )
-def ssh_agent_env(args):
+def ssh_agent_env(args): # pragma: no cover
     args.env_path = pathlib.Path(args.env_path)
     env = dict(parse_environment(args.env_path))
     if not is_agent_up(env, args.safe_run):
